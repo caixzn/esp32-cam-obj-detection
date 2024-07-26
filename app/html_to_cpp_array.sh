e9ce99739953877a541b834a24405be9695e8be5
caio <hi@caio.sh>
2024-07-26 01:51:43 -0300

    feat: use brotli compression

--- app/html_to_cpp_array.sh
+++ app/html_to_cpp_array.sh
@@ -2,9 +2,9 @@ out=include/camera_index.h
 cat /dev/null > $out
 
 for file in app/index_*.html; do
-  buffer=$(gzip -c $file | od -An -t x1 -v -w12 | sed 's/^[[:space:]]*/0x/; s/[[:space:]]*$/,/; s/[[:space:]]/, 0x/g; s/^/    /' | tac | awk '/,$/ && !seen {sub(/,$/, "};"); seen=1} 1' | tac)
+  buffer=$(brotli -c $file | od -An -t x1 -v -w12 | sed 's/^[[:space:]]*/0x/; s/[[:space:]]*$/,/; s/[[:space:]]/, 0x/g; s/^/    /' | tac | awk '/,$/ && !seen {sub(/,$/, "};"); seen=1} 1' | tac)
 
-  echo "#define $(basename $file .html)_html_gz_len $(echo $buffer | wc -w)" >> $out
-  echo "const unsigned char $(basename $file .html)_html_gz[] = {" >> $out
+  echo "#define $(basename $file .html)_html_br_len $(echo $buffer | wc -w)" >> $out
+  echo "const unsigned char $(basename $file .html)_html_br[] = {" >> $out
   echo "$buffer\n" >> $out
 done
--- write_out_buffers.cpp
+++ write_out_buffers.cpp
@@ -3,21 +3,21 @@
 #include "include/camera_index.h"
 
 int main(int argc, char *argv[]) {
-  std::ofstream ofs("index_ov2640.html.gz", std::ios::binary);
+  std::ofstream ofs("index_ov2640.html.br", std::ios::binary);
   if (ofs.is_open()) {
-    ofs.write((const char *)index_ov2640_html_gz, index_ov2640_html_gz_len);
+    ofs.write((const char *)index_ov2640_html_br, index_ov2640_html_br_len);
     ofs.close();
   }
 
-  ofs.open("index_ov3660.html.gz", std::ios::binary);
+  ofs.open("index_ov3660.html.br", std::ios::binary);
   if (ofs.is_open()) {
-    ofs.write((const char *)index_ov3660_html_gz, index_ov3660_html_gz_len);
+    ofs.write((const char *)index_ov3660_html_br, index_ov3660_html_br_len);
     ofs.close();
   }
 
-  ofs.open("index_ov5640.html.gz", std::ios::binary);
+  ofs.open("index_ov5640.html.br", std::ios::binary);
   if (ofs.is_open()) {
-    ofs.write((const char *)index_ov5640_html_gz, index_ov5640_html_gz_len);
+    ofs.write((const char *)index_ov5640_html_br, index_ov5640_html_br_len);
     ofs.close();
   }
 
